4b586f9a12f5c511022334049d693cdf84ab72ed
Devi-s-naidu <117838754+Devi-s-naidu@users.noreply.github.com>
2022-11-17 19:10:38 +0530

    Add files via upload

--- stop clock.py
+++ stop clock.py
@@ -1,6 +1,18 @@
 import time
-n = int(input("enter the seconds"))
-for i in range(n, 0, -1):
-    print(i)
-    time.sleep(1)
-print("time up")
+
+
+def time_convert(seconds):
+    minutes = seconds // 60
+    seconds = seconds % 60
+    hours = minutes // 60
+    minutes = minutes % 60
+    print("time lapsed={0}:{1}:{2}".format(int(hours), int(minutes), seconds))
+
+
+input("press enter to start")
+start_time = time.time()
+input("press enter to stop")
+end_time = time.time()
+
+time_lapsed = end_time - start_time
+time_convert(time_lapsed)
